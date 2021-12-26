2435dad25c08dceba364f54230416e008cf1f743
shri narayan yadav <40333229+shri9098@users.noreply.github.com>
2021-12-26 15:04:58 +0530

    Update settings.py
    issue fixed

--- Forex/settings.py
+++ Forex/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(2ue%q_*2)e1_ei4-si5v7^z)ez5hkm0v+7!wg-xa97jj+e=e1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
